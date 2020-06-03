0c16d0c71a63eaea6f390b9416510cffb20a0ce6
Kelvin Gorospe <kdgorospe@gmail.com>
2020-06-03 12:27:03 -0400

    moved tidy_data function to cleaning_functions.R

--- R_scripts/cleaning_functions.R
+++ R_scripts/cleaning_functions.R
@@ -1,4 +1,7 @@
 # Functions
+
+
+###################################################################################################
 clean_data <- function(df){
   var_labels <- data.frame(col.names = colnames(df), col.labels = NA)
   
@@ -14,4 +17,46 @@ clean_data <- function(df){
     mutate_if(is.labelled, as_factor)
   
   return(list(df = df, var_labels = var_labels))
+}
+
+
+###################################################################################################
+
+tidy_data <- function(df, pivot_col_1, pivot_col_last, var_labels, question_no = TRUE){
+  
+  not_all_na <- function(x) any(!is.na(x))
+  
+  if (question_no == TRUE){
+    # Make tidy
+    df_tidy <- df %>%
+      mutate_all(as.character) %>% # mutate class to character to avoid warning message attributes are not identical across measure variables; they will be dropped
+      pivot_longer(!!pivot_col_1:!!pivot_col_last, names_to = "question", values_to = "response") %>%
+      filter(!is.na(response)) %>%
+      left_join(var_labels, by = c("question" = "col.names")) %>%
+      separate(col.labels, into = c("question.no", "question", "option"), sep = ":") %>%
+      mutate(question = str_trim(question),
+             question.no = tolower(question.no),
+             question = case_when(is.na(question) ~ question.no,
+                                  TRUE ~ question))  %>% # For sex, age, birthday/year, relationship, age in mo.; there is no ":" separator, so copy "question.no" to fill in for "question"
+      select_if(not_all_na)
+    
+    
+    
+  } else {
+    
+    
+    # Make tidy
+    df_tidy <- df %>%
+      mutate_all(as.character) %>% # mutate class to character to avoid warning message attributes are not identical across measure variables; they will be dropped
+      pivot_longer(pivot_col_1:!!pivot_col_last, names_to = "question", values_to = "response") %>%
+      filter(!is.na(response)) %>%
+      left_join(var_labels, by = c("question" = "col.names")) %>%
+      separate(col.labels, into = c("question", "option"), sep = ":") %>%
+      mutate(question = str_trim(question)) %>%
+      select_if(not_all_na)
+    
+  }
+  
+  return(df_tidy)
+  
 }
