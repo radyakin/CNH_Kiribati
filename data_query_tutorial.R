4cfbb13a399ae551478249ff5a5c7459a6753d3d
Kelvin Gorospe <kdgorospe@gmail.com>
2021-06-08 17:07:28 -0400

    add food recall section

--- data_query_tutorial.R
+++ data_query_tutorial.R
@@ -1,5 +1,11 @@
 # Start by loading df_tidy or var_labels
-# Change file.path as needed
+# Change file.path directories as needed
+rm(list=ls())
+library(tidyverse)
+datadir <- "/Volumes/jgephart/Kiribati/Data"
+outdir <- "/Volumes/jgephart/Kiribati/Outputs"
+
+
 df_tidy <- read.csv(file = file.path(outdir, "hies_tidy_individual-level.csv"))
 var_labels <- read.csv(file = file.path(outdir, "hies_question-id-to-label-key.csv"))
 
@@ -83,3 +89,57 @@ df_query <- df_tidy %>%
 
 # save df_query
 # write.csv(df_query, file = "insert-filename-here.csv")
+
+# FIX IT::: add documentation based on call with Mike
+# For food recall questions: i.e., any questions beginning with "In the last 7 days...."
+expend_agg <- read.csv(file.path(outdir, "special-roster-hies_expenditure.csv"))
+
+# Notice there are several sections included here:
+sort(unique(expend_agg$section))
+
+food_recall <- expend_agg %>%
+  filter(section == "21_foodrecall")
+
+# Get list of all possible food descriptions:
+sort(unique(food_recall$description))
+
+list_of_descriptions <- c("Oceanic fish (Tuna, Wahoo, Mahi-mahi, etc.)",
+                          "Reef fish (fresh or frozen)",
+                          "Lagoon and sandflat fish (fresh or frozen)",
+                          "Deep sea fish (fresh or frozen)",
+                          "Fresh water fish",
+                          "Sharks (fresh or frozen)",
+                          "Sea worm",
+                          "Lobster",
+                          "Cockles",
+                          "Clams",
+                          "Sea snails",
+                          "Tinned tuna",
+                          "Tinned mackerel",
+                          "Other dried, canned or salted fish",
+                          "Land crab (mud crab, coconut crab, etc.)"
+                          )
+
+# Use var_labels to get descriptions of columns in food_recall - cn use this to help decide which are relevant to your data query
+var_labels %>%
+  filter(col.names %in% names(food_recall))
+
+h1503bc <- food_recall %>%
+  filter(description %in% list_of_descriptions) %>%
+  select(any_of(c(id_cols,
+                  "amount_corrected",
+                  "amount_def",
+                  "amount_new",
+                  "amount_resell",
+                  "annual_amount",
+                  "annual_amount_clean",
+                  "annual_exp",
+                  "qty",
+                  "qty_corrected", 
+                  "qty_gram", 
+                  "qty_gram_new",
+                  "raw_amount",
+                  "raw_amount_est" ,
+                  "unit")))
+
+expend_labels <- get_var_labels(expend_agg)
