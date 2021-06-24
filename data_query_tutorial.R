ed3588484840fbe1e9713b4cb49d72fb648a29c7
Kelvin Gorospe <kdgorospe@gmail.com>
2021-06-24 16:24:32 -0400

    add food recall notes to tutorial

--- data_query_tutorial.R
+++ data_query_tutorial.R
@@ -90,61 +90,46 @@ df_query <- df_tidy %>%
 # save df_query
 # write.csv(df_query, file = "insert-filename-here.csv")
 
-# FIX IT::: add documentation based on call with Mike
-# For food recall questions: i.e., any questions beginning with "In the last 7 days...."
-expend_agg <- read.csv(file.path(outdir, "special-roster-hies_expenditure.csv"))
+# For food recall questions: i.e., any questions beginning with "In the last 7 days....", query the expenditures dataset
+hies_expenditure <- read.csv(file.path(outdir, "hies_expenditures-standard-units.csv"))
 
 # Notice there are several sections included here:
-sort(unique(expend_agg$section))
+sort(unique(hies_expenditure$section))
 
-food_recall <- expend_agg %>%
+food_recall <- hies_expenditure %>%
   filter(section == "21_foodrecall")
-
-# Get list of all possible food descriptions:
-sort(unique(food_recall$description))
 
 # To get specific types of food recall:
 # (1) Filter by specific coicop_class
 unique(food_recall$coicop_class)
 
+# For question h1503b and h1503c, filter to coicop_class == "Fish and sea food"
+
 # Or (2) Filter by specific responses in the desciprtion column
-list_of_descriptions <- c("Oceanic fish (Tuna, Wahoo, Mahi-mahi, etc.)",
-                          "Reef fish (fresh or frozen)",
-                          "Lagoon and sandflat fish (fresh or frozen)",
-                          "Deep sea fish (fresh or frozen)",
-                          "Fresh water fish",
-                          "Sharks (fresh or frozen)",
-                          "Sea worm",
-                          "Lobster",
-                          "Cockles",
-                          "Clams",
-                          "Sea snails",
-                          "Tinned tuna",
-                          "Tinned mackerel",
-                          "Other dried, canned or salted fish",
-                          "Land crab (mud crab, coconut crab, etc.)"
-                          )
+# list_of_descriptions <- c("Oceanic fish (Tuna, Wahoo, Mahi-mahi, etc.)",
+#                           "Reef fish (fresh or frozen)",
+#                           "Lagoon and sandflat fish (fresh or frozen)",
+#                           "Deep sea fish (fresh or frozen)",
+#                           "Fresh water fish",
+#                           "Sharks (fresh or frozen)",
+#                           "Sea worm",
+#                           "Lobster",
+#                           "Cockles",
+#                           "Clams",
+#                           "Sea snails",
+#                           "Tinned tuna",
+#                           "Tinned mackerel",
+#                           "Other dried, canned or salted fish",
+#                           "Land crab (mud crab, coconut crab, etc.)"
+#                           )
 
 # Use var_labels to get descriptions of columns in food_recall - cn use this to help decide which are relevant to your data query
 var_labels %>%
   filter(col.names %in% names(food_recall))
 
-h1503bc <- food_recall %>%
-  filter(description %in% list_of_descriptions) %>%
-  select(any_of(c(id_cols,
-                  "amount_corrected",
-                  "amount_def",
-                  "amount_new",
-                  "amount_resell",
-                  "annual_amount",
-                  "annual_amount_clean",
-                  "annual_exp",
-                  "qty",
-                  "qty_corrected", 
-                  "qty_gram", 
-                  "qty_gram_new",
-                  "raw_amount",
-                  "raw_amount_est" ,
-                  "unit")))
+h1503b_and_c <- food_recall %>%
+  #filter(description %in% list_of_descriptions) %>% # if filtering by "Description"
+  filter(coicop_class == "Fish and sea food")
 
-expend_labels <- get_var_labels(expend_agg)
+write.csv(h1503b_and_c, file.path(outdir, "Data Requests", "2021-06-24_Data-request_IRS_h1503b_and_c.csv"), row.names = FALSE)
+
